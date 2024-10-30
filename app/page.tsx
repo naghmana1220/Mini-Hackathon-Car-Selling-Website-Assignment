e6d955cf4b551d232038971abeba6563e3bb6227
naghmana1220 <naghmanaasif85@gmail.com>
2024-10-30 12:55:37 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroSection from './Components/HeroSection'
 import Feature from './Components/Feature'
-import Image from 'next/image'
+
 
 
 export default function  Home() {
